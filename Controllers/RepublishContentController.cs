3a07a36189aa09b801e147bb1214d4bfe46ecb23
Milos Mihajlovic <milos.mihajlovic@geta.no>
2019-02-26 19:28:57 +0100

    Menu item, dependencies, initial MVC code

---
+++ Controllers/RepublishContentController.cs
@@ -0,0 +1,25 @@
+﻿using EPiServer.Framework.Localization;
+using EPiServer.Shell;
+using System.Web.Mvc;
+
+namespace AddOn.RepublishContent.Controllers
+{
+    public class RepublishContentController : Controller
+    {
+        public ActionResult Index()
+        {
+            var path = Paths.ToResource(GetType(), "Views/RepublishContent/Index.cshtml");
+            var model = new RepublishModel
+            {
+                Heading = LocalizationService.Current.GetString("/addon.republishcontent/heading")
+            };
+
+            return View(path, model);
+        }
+    }
+
+    public class RepublishModel
+    {
+        public string Heading { get; set; }
+    }
+}
---
+++ Core/RepublishContentMenuProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using EPiServer;
+using EPiServer.Framework.Localization;
+using EPiServer.Security;
+using EPiServer.Shell.Navigation;
+
+namespace AddOn.RepublishContent.Core
+{
+    [MenuProvider]
+    public class RepublishContentMenuProvider : IMenuProvider
+    {
+        public IEnumerable<MenuItem> GetMenuItems()
+        {
+            var menuItems = new List<UrlMenuItem>
+            {
+                new UrlMenuItem(LocalizationService.Current.GetString("/addon.republishcontent/name"),
+                MenuPaths.Global + "/cms" + "/cmsMenuItem",
+                UriSupport.ResolveUrlFromUIAsRelativeOrAbsolute("/RepublishContent/"))
+                {
+                    SortIndex = SortIndex.First + 25,
+                    IsAvailable = (request) => PrincipalInfo.HasAdminAccess
+                }
+            };
+
+            return menuItems;
+        }
+    }
+}

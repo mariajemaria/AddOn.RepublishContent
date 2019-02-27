1694f194b6d2879022d39f9fafb2203151c2fcb2
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2019-02-27 08:38:37 +0100

    Added get method

---
+++ API/Controllers/RepublishContentApiController.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Web.Http;
+using EPiServer.DataAbstraction;
+using EPiServer.ServiceLocation;
+using GetaCommerce.Web.Infrastructure.Admin.Tools.Models;
+using Newtonsoft.Json;
+
+namespace RepublishContentAlloy.Admin.Tools.Controllers.Api
+{
+    //[RoutePrefix("api/republish")]
+    public class RepublishContentApiController : ApiController
+    {
+
+        private readonly IContentTypeRepository _typerepo = ServiceLocator.Current.GetInstance<IContentTypeRepository>();
+
+        // GET api/<controller>
+        public IHttpActionResult GetContentTypes()
+        {
+            var contentTypeDtos = _typerepo.List().Select(ct => new ContentTypeDto
+                {ID = ct.ID, Name = ct.FullName}).ToList();
+
+
+            if (contentTypeDtos.Any())
+            {
+                var settings = new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore
+                };
+
+                settings.NullValueHandling = NullValueHandling.Ignore;
+
+                var contentTypesJson = JsonConvert.SerializeObject(contentTypeDtos, settings);
+
+                return Ok(contentTypesJson);
+            }
+            else
+            {
+                return BadRequest("No content types available");
+            }
+        }
+    }
+}
---
+++ API/Models/ContentTypeDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GetaCommerce.Web.Infrastructure.Admin.Tools.Models
+{
+    public class ContentTypeDto
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        //public string FullName { get; set; }
+    }
+}
